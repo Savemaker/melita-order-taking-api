59a7a9e9ccaf303a54298d902b3bba9de0a9d0ca
ssavushkin <ssavushkin@griddynamics.com>
2022-05-06 17:39:12 +0300

    Adds API info about available packages

--- src/main/java/com/melita/task/application/validation/AtLeastOneProductRequired.java
+++ src/main/java/com/melita/task/application/validation/AtLeastOneProductRequired.java
@@ -15,5 +15,5 @@ public @interface AtLeastOneProductRequired {
 
     Class<? extends Payload>[] payload() default {};
 
-    String message() default "Please specify at least one product";
+    String message() default "Please specify at least one product. Available products: internetPackage={INTERNET_250_MBPS, INTERNET_1_GBPS}, mobilePackage={MOBILE_PREPAID, MOBILE_POSTPAID}, telephonyPackage={TELEPHONY_FREE_ON_NET_CALLS, TELEPHONY_UNLIMITED_CALLS}, tvPackage={TV_90_CHANNELS, TV_140_CHANNELS}";
 }
